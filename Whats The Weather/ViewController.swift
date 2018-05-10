57205b2d9c388de2ecc1c76a8ed77f09d9d1f82c
Unknown <stevelern@gmail.com>
2018-05-09 20:54:06 -0400

    Whats the weather
    its working

--- Whats The Weather/ViewController.swift
+++ Whats The Weather/ViewController.swift
@@ -9,17 +9,93 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var cityTextField: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func getWeatherTapped(_ sender: Any) {
+        
+        if let url = URL (string: "https://www.weather-forecast.com/locations/" + cityTextField.text!.replacingOccurrences(of: " ", with: "-") + "/forecasts/latest") {
+            
+            let request = NSMutableURLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                
+                var message = ""
+                
+                if let error =  error {
+                    
+                    print(error)
+                    
+                } else {
+                    
+                    if let unwrappedData = data {
+                        
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        
+                        /*
+                        var stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                        */
+                        
+                        var stringSeparator = "<p class=\"b-forecast__table-description-content\"><span class=\"phrase\">"
+                        
+ 
+                        if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                            
+                            if contentArray.count > 1 {
+                                
+                                stringSeparator = "</span>"
+                                
+                                let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
+                                
+                                if newContentArray.count > 1 {
+                                    
+                                    message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                    
+                                    print(message)
+                                    
+                                }
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+                if message == "" {
+                    
+                    message = "The weather there couldn't be found. Please try again."
+                    
+                }
+                
+                DispatchQueue.main.sync(execute: {
+                    
+                    self.resultLabel.text = message
+                    
+                })
+                
+            }
+            
+            task.resume()
+            
+        } else {
+            
+            resultLabel.text = "The weather there couldn't be found. Please try again."
+            
+        }
+        
+        
+        
+        
     }
-
-
+    
 }
 
